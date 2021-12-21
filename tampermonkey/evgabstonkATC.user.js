7fba824de574624b2f19a4a15a3d3d2ccd4de2b1
Twitch Krowve <twitch.krowve@gmail.com>
2021-12-21 20:33:38 +0000

    Cart from b stock page

---
+++ tampermonkey/evgabstonkATC.user.js
@@ -0,0 +1,72 @@
+// ==UserScript==
+// @name         evgabstonkATC
+// @namespace    http://tampermonkey.net/
+// @version      2021211001
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.evga.com/*
+// @match        https://secure.evga.com/*
+// @icon         https://www.google.com/s2/favicons?domain=evga.com
+// @grant        none
+// ==/UserScript==
+
+
+
+
+
+(function() {
+    'use strict';
+
+    // Will cart the first card it sees, change order based on your preference.
+
+var PRODUCTS = new Array()
+
+// 3060 Ti XC Gaming
+PRODUCTS.push("08G-P5-3663-RX")
+// 3060 Ti FTW3 Gaming
+PRODUCTS.push("08G-P5-3665-RX")
+// 3060 Ti FTW3 Ultra Gaming
+PRODUCTS.push("08G-P5-3667-RX")
+// 3070 XC3 Black Gaming
+PRODUCTS.push("08G-P5-3751-RX")
+// 3070 XC3 Ultra Gaming
+PRODUCTS.push("08G-P5-3755-RX")
+// 3070 FTW3 Ultra Gaming
+PRODUCTS.push("08G-P5-3767-RX")
+// 3080 XC3 Black Gaming
+PRODUCTS.push("10G-P5-3881-RX")
+// 3080 XC3 Gaming
+PRODUCTS.push("10G-P5-3883-RX")
+// 3080 XC3 Ultra Gaming
+PRODUCTS.push("10G-P5-3885-RX")
+// 3080 FTW3 Gaming
+PRODUCTS.push("10G-P5-3895-RX")
+// 3080 FTW3 Ultra Gaming
+PRODUCTS.push("10G-P5-3897-RX")
+PRODUCTS.push("10G-P5-3885-RX")
+// 3080 FTW3 Gaming
+PRODUCTS.push("10G-P5-3895-RX")
+// 3080 FTW3 Ultra Gaming
+PRODUCTS.push("10G-P5-3897-RX")
+// 3090 FTW3 Gaming
+PRODUCTS.push("24G-P5-3985-RX")
+// 3090 FTW3 Ultra Gaming
+PRODUCTS.push("24G-P5-3987-RX")
+
+// 3090 FTW3 Ultra Hybrid Gaming
+// PRODUCTS.push("24G-P5-3988-RX")
+
+// EVGA GeForce 210 DDR3
+//PRODUCTS.push("01G-P3-1313-RX")
+
+     if (document.title.includes("EVGA - Products - B-Stock")) {
+         var PRODUCT = ""
+         for (var i = 0; i < PRODUCTS.length; i++) {
+             PRODUCT = PRODUCTS[i]
+
+             if (document.querySelector("[title*='" + PRODUCT + "']")) {
+                 document.querySelector("[title*='" + PRODUCT + "']").click()
+             }
+         }
+     }
+})();
