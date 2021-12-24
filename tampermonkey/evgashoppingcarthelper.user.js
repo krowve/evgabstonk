03be32596bedd7edf306427306ab6937751b18f2
Twitch Krowve <twitch.krowve@gmail.com>
2021-12-24 17:38:42 +0000

    limit where this runs

--- tampermonkey/evgashoppingcarthelper.user.js
+++ tampermonkey/evgashoppingcarthelper.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         evgashoppingcarthelper
 // @namespace    http://tampermonkey.net/
-// @version      2021121101
+// @version      2021122401
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.evga.com/*
-// @match        https://secure.evga.com/*
+// @downloadURL  https://github.com/krowve/evgabstonk/raw/main/tampermonkey/evgashoppingcarthelper.user.js
+// @updateURL    https://github.com/krowve/evgabstonk/raw/main/tampermonkey/evgashoppingcarthelper.user.js
+// @match        https://www.evga.com/products/*
+// @match        https://www.evga.com/Products/*
 // @icon         https://www.google.com/s2/favicons?domain=evga.com
 // @grant        none
 // ==/UserScript==
