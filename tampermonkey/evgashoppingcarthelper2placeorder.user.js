01d846bbf36f57c7dbd66663ad661efaf4a91e8a
Twitch Krowve <twitch.krowve@gmail.com>
2022-01-10 21:42:26 -0500

    go straight to place order from shopping cart

---
+++ tampermonkey/evgashoppingcarthelper2placeorder.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         evgashoppingcarthelper2placeorder
+// @namespace    http://tampermonkey.net/
+// @version      2022011001
+// @description  try to take over the world!
+// @author       You
+// @downloadURL  https://github.com/krowve/evgabstonk/raw/main/tampermonkey/evgashoppingcarthelper.user.js
+// @updateURL    https://github.com/krowve/evgabstonk/raw/main/tampermonkey/evgashoppingcarthelper.user.js
+// @match        https://www.evga.com/products/shoppingcart.aspx
+// @match        https://www.evga.com/Products/shoppingcart.aspx
+// @match        https://www.evga.com/Products/ShoppingCart.aspx
+// @icon         https://www.google.com/s2/favicons?domain=evga.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+        if (document.title == "EVGA - Checkout") {
+            if (!(document.getElementsByClassName('message-important')[0].innerText == "Your Cart is Empty")) {
+                console.log("Checkout")
+                //document.getElementsByClassName('btn-checkout')[0].click()
+                setTimeout(function(){ document.location = "https://secure.evga.com/cart/checkout_placeorder.aspx" }, 1000);
+            }
+
+        }
+
+})();
