73382646b150b2b02a8409b1908fc46a867ca94a
Twitch Krowve <twitch.krowve@gmail.com>
2022-01-18 19:27:21 -0500

    adding another match

--- tampermonkey/evgashoppingcarthelper2placeorder.user.js
+++ tampermonkey/evgashoppingcarthelper2placeorder.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         evgashoppingcarthelper2placeorder
 // @namespace    http://tampermonkey.net/
-// @version      2022011101
+// @version      20220118-1
 // @description  try to take over the world!
 // @author       You
 // @downloadURL  https://github.com/krowve/evgabstonk/raw/main/tampermonkey/evgashoppingcarthelper.user.js
 // @updateURL    https://github.com/krowve/evgabstonk/raw/main/tampermonkey/evgashoppingcarthelper.user.js
 // @match        https://www.evga.com/products/shoppingcart.aspx
+// @match        https://www.evga.com/products/ShoppingCart.aspx
 // @match        https://www.evga.com/Products/shoppingcart.aspx
 // @match        https://www.evga.com/Products/ShoppingCart.aspx
 // @icon         https://www.google.com/s2/favicons?domain=evga.com
