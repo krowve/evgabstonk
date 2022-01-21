969c2dbdbc6b9eff3e8d06ec480d189c0105f32b
vcbb10 <34849357+vcbb10@users.noreply.github.com>
2022-01-21 11:03:42 -0600

    Update with Card Price, ETH MH and ETH Watts.

--- tampermonkey/evgabstonkATC.user.js
+++ tampermonkey/evgabstonkATC.user.js
@@ -21,154 +21,229 @@
 
 var PRODUCTS = new Array()
 // 3080 XC3 Black Gaming
+// $800 - 92MH - 230w
 PRODUCTS.push("10G-P5-3881-RX")
 // 3080 XC3 Gaming
+// $850 - 92MH - 230w
 PRODUCTS.push("10G-P5-3883-RX")
 // 3080 XC3 Ultra Gaming
+// $870 - 92MH - 230w
 PRODUCTS.push("10G-P5-3885-RX")
 // 3080 FTW3 Gaming
+// $870 - 92MH - 230w
 PRODUCTS.push("10G-P5-3895-RX")
 // 3080 FTW3 Ultra Gaming
-PRODUCTS.push("10G-P5-3897-RX")    
+// $910 - 92MH - 230w
+PRODUCTS.push("10G-P5-3897-RX")
 // 3060 Ti XC Gaming
+// $470 - 59MH - 130w
 PRODUCTS.push("08G-P5-3663-RX")
 // 3060 Ti FTW3 Gaming
+// $500 - 59MH - 130w
 PRODUCTS.push("08G-P5-3665-RX")
 // 3060 Ti FTW3 Ultra Gaming
+// $530 - 59MH - 130w
 PRODUCTS.push("08G-P5-3667-RX")
 // 3070 XC3 Black Gaming
+// $600 - 59MH - 130w
 PRODUCTS.push("08G-P5-3751-RX")
 // 3070 XC3 Ultra Gaming
+// $650 - 59MH - 130w
 PRODUCTS.push("08G-P5-3755-RX")
 // 3070 FTW3 Ultra Gaming
+// $710 - 59MH - 130w
 PRODUCTS.push("08G-P5-3767-RX")
 // 3080 XC3 Black Gaming
+// $800 - 92MH - 230w
 PRODUCTS.push("10G-P5-3881-RX")
 // 3080 XC3 Gaming
+// $850 - 92MH - 230w
 PRODUCTS.push("10G-P5-3883-RX")
 // 3080 XC3 Ultra Gaming
+// $870 - 92MH - 230w
 PRODUCTS.push("10G-P5-3885-RX")
 // 3080 FTW3 Gaming
+// $870 - 92MH - 230w
 PRODUCTS.push("10G-P5-3895-RX")
 // 3080 FTW3 Ultra Gaming
+// $910 - 92MH - 230w
 PRODUCTS.push("10G-P5-3897-RX")
 // 3060 XC Black Gaming
+// $390 - 34MH - 120w
 PRODUCTS.push("12G-P5-3655-RX")
 // 3060 XC Gaming
+// $420 - 34MH - 120w
 PRODUCTS.push("12G-P5-3657-RX")
 // 3090 XC3 Gaming
-PRODUCTS.push("24G-P5-3973-RX")    
+// $1650 - 114MH - 320w
+PRODUCTS.push("24G-P5-3973-RX")
 // 3090 FTW3 Gaming
+// $1830 - 114MH - 320w
 PRODUCTS.push("24G-P5-3985-RX")
 // 3090 XC3 Ultra Gaming
-PRODUCTS.push("24G-P5-3975-RX")    
+// $1730 - 114MH - 320w
+PRODUCTS.push("24G-P5-3975-RX")
 // 3090 FTW3 Ultra Gaming
+// $1900 - 114MH - 320w
 PRODUCTS.push("24G-P5-3987-RX")
 // 3060Ti XC Gaming LHR
-PRODUCTS.push("08G-P5-3663-RL")    
+// $470 - 42MH - 130w
+PRODUCTS.push("08G-P5-3663-RL")
 // 3060Ti FTW Ultra Gaming LHR
+// $530 - 42MH - 130w
 PRODUCTS.push("08G-P5-3667-RL")
 // 3070 Ti FTW3 Ultra Gaming
+// $820 - 55MH - 190w
 PRODUCTS.push("08G-P5-3797-RX")
 // 3090 XC3 Gaming
-PRODUCTS.push("24G-P5-3973-RX")    
+// $1650 - 114MH - 320w
+PRODUCTS.push("24G-P5-3973-RX")
 // 3090 FTW3 Gaming
+// $1830 - 114MH - 320w
 PRODUCTS.push("24G-P5-3985-RX")
 // 3090 XC3 Ultra Gaming
-PRODUCTS.push("24G-P5-3975-RX")    
+// $1730 - 114MH - 320w
+PRODUCTS.push("24G-P5-3975-RX")
 // 3090 FTW3 Ultra Gaming
+// $1900 - 114MH - 320w
 PRODUCTS.push("24G-P5-3987-RX")
 // 3080 XC3 Black Gaming LHR
-PRODUCTS.push("10G-P5-3881-RL")    
+// $800 - 71MH - 240w
+PRODUCTS.push("10G-P5-3881-RL")
 // 3080 XC3 Ultra Gaming LHR
+// $870 - 71MH - 240w
 PRODUCTS.push("10G-P5-3885-RL")
 // 3080 Ti XC3 Gaming
-PRODUCTS.push("12G-P5-3953-RX")    
+// $1190 - 85MH - 290w
+PRODUCTS.push("12G-P5-3953-RX")
 // 3080 Ti XC3 Ultra Gaming
-PRODUCTS.push("12G-P5-3955-RX")    
+// $1310 - 85MH - 290w
+PRODUCTS.push("12G-P5-3955-RX")
 // 3080 Ti XC3 Ultra Hybrid Gaming
+// $1410 - 85MH - 290w
 PRODUCTS.push("12G-P5-3958-RX")
 // 3080 Ti FTW3 Ultra Gaming
+// $1410 - 85MH - 290w
 PRODUCTS.push("12G-P5-3967-RX")
 // 3070 XC3 Ultra Gaming LHR
-PRODUCTS.push("08G-P5-3755-RL")    
+// $650 - 43MH - 130w
+PRODUCTS.push("08G-P5-3755-RL")
 // 3090 FTW3 Ultra Hybrid Gaming
+// $1900 - 114MH - 320w
 PRODUCTS.push("24G-P5-3988-RX")
 
 // 2060 KO Gaming
+// $ - 30MH - 120w
 PRODUCTS.push("06G-P4-2066-RX")
 // 2060 KO Ultra Gaming
+// $ - 30MH - 120w
 PRODUCTS.push("06G-P4-2068-RX")
 // 2060 Gaming
+// $ - 30MH - 120w
 PRODUCTS.push("06G-P4-2060-RX")
 // 2060 XC Black Gaming
-PRODUCTS.push("06G-P4-2061-RX")    
+// $ - 30MH - 120w
+PRODUCTS.push("06G-P4-2061-RX")
 // 2060 SC Ultra Gaming
+// $ - 30MH - 120w
 PRODUCTS.push("06G-P4-2067-RX")
 // 2060 SC Black Gaming
+// $ - 30MH - 120w
 PRODUCTS.push("06G-P4-1762-RX")
 // 2060 SC Gaming
-PRODUCTS.push("06G-P4-2062-RX")    
+// $ - 30MH - 120w
+PRODUCTS.push("06G-P4-2062-RX")
 // 2060 XC Gaming
-PRODUCTS.push("06G-P4-2063-RX")        
+// $ - 30MH - 120w
+PRODUCTS.push("06G-P4-2063-RX")
+// $ - 30MH - 120w
 // 2060 SC Ultra Black Gaming
-PRODUCTS.push("06G-P4-2065-RX")    
+PRODUCTS.push("06G-P4-2065-RX")
+// $ - 30MH - 120w
 // 2060 XC Ultra Gaming
 PRODUCTS.push("06G-P4-2167-RX")
 // 2060 XC Ultra Black Gaming
-PRODUCTS.push("06G-P4-2163-RX")    
+// $ - 30MH - 120w
+PRODUCTS.push("06G-P4-2163-RX")
 // 2060 XC Ultra Gaming
+// $ - 30MH - 120w
 PRODUCTS.push("06G-P4-2166-RX")
 // 2060 Super SC Ultra Gaming
+// $ - 40MH - 120w
 PRODUCTS.push("08G-P4-3067-RX")
+// $ - 40MH - 120w
 // 2060 Super Black Gaming Dual HDB Fans
 PRODUCTS.push("08G-P4-3061-RX")
+// $ - 40MH - 120w
 // 2060 Super Black Gaming Dual Fans
 PRODUCTS.push("08G-P4-3062-RX")
 // 2060 Super SC Ultra Black Gaming
+// $ - 40MH - 120w
 PRODUCTS.push("08G-P4-3065-RX")
 // 2060 Super XC Black Gaming
-PRODUCTS.push("08G-P4-3161-RX")    
+// $ - 40MH - 120w
+PRODUCTS.push("08G-P4-3161-RX")
 // 2060 Super XC Gaming
-PRODUCTS.push("08G-P4-3162-RX")    
+// $ - 40MH - 120w
+PRODUCTS.push("08G-P4-3162-RX")
 // 2060 Super XC Ultra Gaming
-PRODUCTS.push("08G-P4-3163-RX")    
+// $ - 40MH - 120w
+PRODUCTS.push("08G-P4-3163-RX")
 // 2070 Black Gaming
+// $ - 40MH - 140w
 PRODUCTS.push("08G-P4-1071-RX")
 // 2070 XC Gaming
+// $ - 40MH - 140w
 PRODUCTS.push("08G-P4-2172-RX")
 // 2070 XC Ultra Gaming
+// $ - 40MH - 140w
 PRODUCTS.push("08G-P4-2173-RX")
 // 2070 XC Black Edition
+// $ - 40MH - 140w
 PRODUCTS.push("08G-P4-2071-RX")
 // 2070 Super Black Gaming
+// $ - 41MH - 140w
 PRODUCTS.push("08G-P4-3071-RX")
 // 2070 XC Black Edition Gaming
-PRODUCTS.push("08G-P4-1171-RX")    
+// $ - 40MH - 140w
+PRODUCTS.push("08G-P4-1171-RX")
 // 2070 FTW3 Gaming
+// $ - 40MH - 140w
 PRODUCTS.push("08G-P4-2273-RX")
 // 2070 FTW3 Ultra iCX2
+// $ - 40MH - 140w
 PRODUCTS.push("08G-P4-2277-RX")
 // 2070 Super KO Gaming
+// $ - 41MH - 140w
 PRODUCTS.push("08G-P4-2072-RX")
 // 2070 Super Gaming
+// $ - 41MH - 140w
 PRODUCTS.push("08G-P4-3070-RX")
 // 2070 Super XC Gaming
+// $ - 41MH - 140w
 PRODUCTS.push("08G-P4-3172-RX")
 // 2070 Gaming
+// $ - 40MH - 140w
 PRODUCTS.push("08G-P4-2070-RX")
 // 2070 Super XC Ultra Gaming
+// $ - 41MH - 140w
 PRODUCTS.push("08G-P4-3173-RX")
 // 2070 Super FTW3 Gaming
+// $ - 41MH - 140w
 PRODUCTS.push("08G-P4-3273-RX")
 // 2070 Super FTW3 Ultra Gaming
+// $ - 41MH - 140w
 PRODUCTS.push("08G-P4-3277-RX")
 // 2070 Super FTW3 Ultra+
+// $ - 41MH - 140w
 PRODUCTS.push("08G-P4-3377-RX")
 // 2070 XC Black Gaming
+// $ - 40MH - 140w
 PRODUCTS.push("08G-P4-2171-RX")
 // 2070 Super XC Ultra+
+// $ - 41MH - 140w
 PRODUCTS.push("08G-P4-3175-RX")
+
 // EVGA GeForce 210 DDR3
 //PRODUCTS.push("01G-P3-1313-RX")
 
