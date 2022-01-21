e23f03be2d7ae95505f8676be383054fd9a6b9d6
vcbb10 <34849357+vcbb10@users.noreply.github.com>
2022-01-21 10:52:09 -0600

    Update evgabstonkATC16only.user.js

--- tampermonkey/evgabstonkATC16only.user.js
+++ tampermonkey/evgabstonkATC16only.user.js
@@ -17,44 +17,61 @@
 
 // Will try to cart in listed order.
 // Will only try to cart one card per page load.
-// Sort by your preference.  
+// Sort by your preference.
 
 var PRODUCTS = new Array()
-// EVGA GeForce GTX 1660 Ti XC Ultra Gaming 
+// EVGA GeForce GTX 1660 Ti XC Ultra Gaming
+// $300 - 28MH - 80w
 PRODUCTS.push("06G-P4-1266-RX")
 // EVGA GeForce GTX 1660 Ti SC Ultra Black Gaming
+// $290 - 28MH - 80w
 PRODUCTS.push("06G-P4-1655-RX")
 // EVGA GeForce GTX 1660 Ti XC Ultra Black Gaming
+// $280 - 28MH - 80w
 PRODUCTS.push("06G-P4-1265-RX")
 // EVGA GeForce GTX 1660 Ti XC Ultra Gaming
+// $280 - 28MH - 80w
 PRODUCTS.push("06G-P4-1267-RX")
 // EVGA GeForce GTX 1660 Ti SC Ultra Gaming
+// $270 - 28MH - 80w
 PRODUCTS.push("06G-P4-1667-RX")
 // EVGA GeForce GTX 1660 Ti XC Black Gaming
+// $260 - 28MH - 80w
 PRODUCTS.push("06G-P4-1261-RX")
 // EVGA GeForce GTX 1660 Ti XC Gaming
+// $250 - 28MH - 80w
 PRODUCTS.push("06G-P4-1263-RX")
 // EVGA GeForce GTX 1660 XC Ultra Gaming
+// $230 - 23MH - 80w
 PRODUCTS.push("06G-P4-1167-RX")
 // EVGA GeForce GTX 1660 SUPER SC Ultra Black Gaming
+// $220 - 29MH - 90w
 PRODUCTS.push("06G-P4-1066-RX")
 // EVGA GeForce GTX 1660 SC Ultra Gaming
+// $220 - 23MH - 80w
 PRODUCTS.push("06G-P4-1067-RX")
 // EVGA GeForce GTX 1660 SUPER SC Ultra Gaming
+// $220 - 29MH - 90w
 PRODUCTS.push("06G-P4-1068-RX")
 // EVGA GeForce GTX 1660 XC Ultra Black Gaming
+// $220 - 23MH - 80w
 PRODUCTS.push("06G-P4-1165-RX")
 // EVGA GeForce GTX 1660 SUPER BLACK GAMING
+// $210 - 29MH - 90w
 PRODUCTS.push("06G-P4-1061-RX")
 // EVGA GeForce GTX 1660 XC Black Gaming
+// $210 - 23MH - 80w
 PRODUCTS.push("06G-P4-1161-RX")
 // EVGA GeForce GTX 1660 XC Gaming
+// $210 - 23MH - 80w
 PRODUCTS.push("06G-P4-1163-RX")
 // EVGA GeForce GTX 1660 SC Ultra Black Gaming
+// $200 - 23MH - 80w
 PRODUCTS.push("06G-P4-1065-RX")
 // EVGA GeForce GTX 1660 Black Gaming
-PRODUCTS.push("06G-P4-1160-RX")    
-    
+// $200 - 23MH - 80w
+PRODUCTS.push("06G-P4-1160-RX")
+
 // EVGA GeForce 210 DDR3
 //PRODUCTS.push("01G-P3-1313-RX")
 
