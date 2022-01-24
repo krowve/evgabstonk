60e36d23cbc868506720fec147c6f649a43ea5f4
vcbb10 <34849357+vcbb10@users.noreply.github.com>
2022-01-24 17:41:22 -0600

    Add missing 20 series pricing

--- tampermonkey/evgabstonkATC.user.js
+++ tampermonkey/evgabstonkATC.user.js
@@ -20,6 +20,7 @@
 // Sort by your preference.  
 
 var PRODUCTS = new Array()
+// 30 Series
 // 3080 XC3 Black Gaming
 // $800 - 92MH - 230w
 PRODUCTS.push("10G-P5-3881-RX")
@@ -132,117 +133,119 @@ PRODUCTS.push("08G-P5-3755-RL")
 // $1900 - 114MH - 320w
 PRODUCTS.push("24G-P5-3988-RX")
 
+// 20 Series
 // 2060 KO Gaming
-// $ - 30MH - 120w
+// $280 - 30MH - 120w
 PRODUCTS.push("06G-P4-2066-RX")
 // 2060 KO Ultra Gaming
-// $ - 30MH - 120w
+// $290 - 30MH - 120w
 PRODUCTS.push("06G-P4-2068-RX")
 // 2060 Gaming
-// $ - 30MH - 120w
+// $320 - 30MH - 120w
 PRODUCTS.push("06G-P4-2060-RX")
 // 2060 XC Black Gaming
-// $ - 30MH - 120w
+// $320 - 30MH - 120w
 PRODUCTS.push("06G-P4-2061-RX")
 // 2060 SC Ultra Gaming
-// $ - 30MH - 120w
+// $340 - 30MH - 120w
 PRODUCTS.push("06G-P4-2067-RX")
 // 2060 SC Black Gaming
-// $ - 30MH - 120w
+// $340 - 30MH - 120w
 PRODUCTS.push("06G-P4-1762-RX")
 // 2060 SC Gaming
-// $ - 30MH - 120w
+// $340 - 30MH - 120w
 PRODUCTS.push("06G-P4-2062-RX")
 // 2060 XC Gaming
-// $ - 30MH - 120w
+// $350 - 30MH - 120w
 PRODUCTS.push("06G-P4-2063-RX")
-// $ - 30MH - 120w
 // 2060 SC Ultra Black Gaming
+// $350 - 30MH - 120w
 PRODUCTS.push("06G-P4-2065-RX")
-// $ - 30MH - 120w
 // 2060 XC Ultra Gaming
+// $350 - 30MH - 120w
 PRODUCTS.push("06G-P4-2167-RX")
 // 2060 XC Ultra Black Gaming
-// $ - 30MH - 120w
+// $370 - 30MH - 120w
 PRODUCTS.push("06G-P4-2163-RX")
 // 2060 XC Ultra Gaming
-// $ - 30MH - 120w
+// $370 - 30MH - 120w
 PRODUCTS.push("06G-P4-2166-RX")
 // 2060 Super SC Ultra Gaming
-// $ - 40MH - 120w
+// $390 - 40MH - 120w
 PRODUCTS.push("08G-P4-3067-RX")
-// $ - 40MH - 120w
 // 2060 Super Black Gaming Dual HDB Fans
+// $400 - 40MH - 120w
 PRODUCTS.push("08G-P4-3061-RX")
-// $ - 40MH - 120w
 // 2060 Super Black Gaming Dual Fans
+// $400 - 40MH - 120w
 PRODUCTS.push("08G-P4-3062-RX")
 // 2060 Super SC Ultra Black Gaming
-// $ - 40MH - 120w
+// $400 - 40MH - 120w
 PRODUCTS.push("08G-P4-3065-RX")
 // 2060 Super XC Black Gaming
-// $ - 40MH - 120w
+// $410 - 40MH - 120w
 PRODUCTS.push("08G-P4-3161-RX")
 // 2060 Super XC Gaming
-// $ - 40MH - 120w
+// $430 - 40MH - 120w
 PRODUCTS.push("08G-P4-3162-RX")
 // 2060 Super XC Ultra Gaming
-// $ - 40MH - 120w
+// $440 - 40MH - 120w
 PRODUCTS.push("08G-P4-3163-RX")
 // 2070 Black Gaming
-// $ - 40MH - 140w
+// $450 - 40MH - 140w
 PRODUCTS.push("08G-P4-1071-RX")
 // 2070 XC Gaming
-// $ - 40MH - 140w
+// $450 - 40MH - 140w
 PRODUCTS.push("08G-P4-2172-RX")
 // 2070 XC Ultra Gaming
-// $ - 40MH - 140w
+// $460 - 40MH - 140w
 PRODUCTS.push("08G-P4-2173-RX")
 // 2070 XC Black Edition
-// $ - 40MH - 140w
+// $480 - 40MH - 140w
 PRODUCTS.push("08G-P4-2071-RX")
 // 2070 Super Black Gaming
-// $ - 41MH - 140w
+// $480 - 41MH - 140w
 PRODUCTS.push("08G-P4-3071-RX")
 // 2070 XC Black Edition Gaming
-// $ - 40MH - 140w
+// $490 - 40MH - 140w
 PRODUCTS.push("08G-P4-1171-RX")
 // 2070 FTW3 Gaming
-// $ - 40MH - 140w
+// $490 - 40MH - 140w
 PRODUCTS.push("08G-P4-2273-RX")
 // 2070 FTW3 Ultra iCX2
-// $ - 40MH - 140w
+// $490 - 40MH - 140w
 PRODUCTS.push("08G-P4-2277-RX")
 // 2070 Super KO Gaming
-// $ - 41MH - 140w
+// $500 - 41MH - 140w
 PRODUCTS.push("08G-P4-2072-RX")
 // 2070 Super Gaming
-// $ - 41MH - 140w
+// $500 - 41MH - 140w
 PRODUCTS.push("08G-P4-3070-RX")
 // 2070 Super XC Gaming
-// $ - 41MH - 140w
+// $510 - 41MH - 140w
 PRODUCTS.push("08G-P4-3172-RX")
 // 2070 Gaming
-// $ - 40MH - 140w
+// $530 - 40MH - 140w
 PRODUCTS.push("08G-P4-2070-RX")
 // 2070 Super XC Ultra Gaming
-// $ - 41MH - 140w
+// $530 - 41MH - 140w
 PRODUCTS.push("08G-P4-3173-RX")
 // 2070 Super FTW3 Gaming
-// $ - 41MH - 140w
+// $540 - 41MH - 140w
 PRODUCTS.push("08G-P4-3273-RX")
 // 2070 Super FTW3 Ultra Gaming
-// $ - 41MH - 140w
+// $540 - 41MH - 140w
 PRODUCTS.push("08G-P4-3277-RX")
 // 2070 Super FTW3 Ultra+
-// $ - 41MH - 140w
+// $540 - 41MH - 140w
 PRODUCTS.push("08G-P4-3377-RX")
 // 2070 XC Black Gaming
-// $ - 40MH - 140w
+// $550 - 40MH - 140w
 PRODUCTS.push("08G-P4-2171-RX")
 // 2070 Super XC Ultra+
-// $ - 41MH - 140w
+// $550 - 41MH - 140w
 PRODUCTS.push("08G-P4-3175-RX")
+
 
 // EVGA GeForce 210 DDR3
 //PRODUCTS.push("01G-P3-1313-RX")
