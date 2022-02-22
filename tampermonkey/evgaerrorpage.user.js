cd551ef8ad74d02aca78f5887f3eefea28c1a78a
krowve <84995512+krowve@users.noreply.github.com>
2022-02-22 16:56:52 -0500

    Update evgaerrorpage.user.js
    if error page try to go back to b stock page

--- tampermonkey/evgaerrorpage.user.js
+++ tampermonkey/evgaerrorpage.user.js
@@ -1,1 +1,23 @@
-#
+// ==UserScript==
+// @name         evgaberrorpage
+// @namespace    http://tampermonkey.net/
+// @version      2022010301
+// @description  Cart from the EVGA B Stock Page
+// @author       Krowve
+// @downloadURL  https://github.com/krowve/evgabstonk/raw/main/tampermonkey/evgaerrorpage.user.js
+// @updateURL    https://github.com/krowve/evgabstonk/raw/main/tampermonkey/evgaerrorpage.user.js
+// @match        https://www.evga.com/Products/*
+// @match        https://www.evga.com/products/*
+// @icon         https://www.google.com/s2/favicons?domain=evga.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+     if (document.title.includes("Error")) {
+         setTimeout(function(){ window.open(window.location = "https://www.evga.com/products/productlist.aspx?type=8") }, 5000);
+     }
+})();
+
